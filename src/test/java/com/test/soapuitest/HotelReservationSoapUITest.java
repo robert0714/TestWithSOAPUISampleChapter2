c5750bd9c7c7c36317fe3b4f2bf0ef265dec5a9a
robert <robert0714@hotmail.com>
2015-09-08 18:52:07 +0800

    test fix issues

---
+++ src/test/java/com/test/soapuitest/HotelReservationSoapUITest.java
@@ -0,0 +1,27 @@
+///*
+// * Copyright (c) 2010-2020 IISI.
+// * All rights reserved.
+// *
+// * This software is the confidential and proprietary information of IISI.
+// */
+//package com.test.soapuitest;
+//
+//import com.eviware.soapui.tools.SoapUITestCaseRunner;
+//
+//import junit.framework.TestCase;
+//
+///**
+// *
+// */
+//public class HotelReservationSoapUITest extends TestCase {
+//	public void testSoapUITestRunner() {
+//		SoapUITestCaseRunner soapUITestCaseRunner = new SoapUITestCaseRunner();
+//		soapUITestCaseRunner
+//				.setProjectFile("/home/charitha/soapui-projects/HotelReservationProject-soapui-project.xml");
+//		try {
+//			soapUITestCaseRunner.run();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
